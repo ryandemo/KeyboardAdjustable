494173a07d0692a11f49a8b707b213f42a558141
Ryan Demo <ryan.demo@me.com>
2016-10-25 11:56:53 -0400

    Initial Commit

---
+++ KeyboardAdjustable/KeyboardAdjustable.h
@@ -0,0 +1,19 @@
+//
+//  KeyboardAdjustable.h
+//  KeyboardAdjustable
+//
+//  Created by Ryan Demo on 10/25/16.
+//  Copyright © 2016 RyanDemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KeyboardAdjustable.
+FOUNDATION_EXPORT double KeyboardAdjustableVersionNumber;
+
+//! Project version string for KeyboardAdjustable.
+FOUNDATION_EXPORT const unsigned char KeyboardAdjustableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeyboardAdjustable/PublicHeader.h>
+
+
